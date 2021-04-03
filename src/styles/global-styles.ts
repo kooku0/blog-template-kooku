ac1ace5454f357e2d912b088dc202eca989d9886
kooku <koomg9599@gmail.com>
2021-04-04 02:53:33 +0900

    style: fix sticky error at iphone

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -7,6 +7,17 @@ export const GlobalStyle = () => css`
     margin: 0;
     padding: 0;
     border: 0;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+  }
+
+  #__next {
+    width: 100vw;
+    height: 100vh;
+    overflow-y: scroll;
+    -webkit-overflow-scrolling: touch;
   }
 
   html, body, div, span, applet, object, iframe,
